8a9615f4e48106790f0e9d3d06ceaa04c8349546
Mariya Belinskaya <mv.belinskaya@gmail.com>
2024-11-05 16:09:32 +0500

    add parser.js

---
+++ src/parser.js
@@ -0,0 +1,24 @@
+export default (data) => {
+    const newParser = new DOMParser();
+    const newData = newParser.parseFromString(data, 'application/xml');
+    const parsererror = newData.querySelector('parsererror');
+    if (parsererror) {
+      const error = new Error(parsererror.textContent);
+      error.isParserError = true;
+      throw error;
+    }
+    const feedTitle = newData.querySelector('channel title').textContent;
+    const feedDescription = newData.querySelector('channel description').textContent;
+    const feed = {
+      title: feedTitle,
+      description: feedDescription,
+    };
+    const items = newData.querySelectorAll('item');
+    const list = Array.from(items);
+    const posts = list.map((item) => ({
+      title: item.querySelector('title').textContent,
+      description: item.querySelector('description').textContent,
+      link: item.querySelector('link').textContent,
+    }));
+    return [feed, posts];
+  };
